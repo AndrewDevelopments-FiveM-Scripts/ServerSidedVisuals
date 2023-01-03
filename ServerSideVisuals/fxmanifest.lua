38d77489d727633c02f179f5c1e60b3c728fb91c
AndrewTheWusky <121670301+AndrewTheWusky@users.noreply.github.com>
2023-01-03 15:29:09 +1000

    Added asset to repo.
    added the FiveM asset to the repository.

---
+++ ServerSideVisuals/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "ServerSideVisuals"
+description "This is a server sided visuals pack"
+author "AndrewDevelopment"
+version "1.0.0"
+
+files {
+    'stream/*.*'
+}
